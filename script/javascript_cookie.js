0dfa80dde54f88cee7e252cd7bee17f418769b98
heejun <rabbity9241@gamil.com>
2024-05-23 18:29:21 +0900

    0523

---
+++ script/javascript_cookie.js
@@ -0,0 +1,26 @@
+function setCookie(cookieName, value, exdays){  //쿠키 생성하기
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + exdays);
+        var cookieValue = escape(value) + ((exdays==null) ? "" : "; expires=" + exdate.toGMTString());
+        document.cookie = cookieName + "=" + cookieValue;
+    }
+    
+    function deleteCookie(cookieName){  // 쿠키 지우기
+      var expireDate = new Date();
+      expireDate.setDate(expireDate.getDate() - 1);
+      document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+    }
+      
+    function getCookie(cookieName) {  // 쿠키 가져오기
+      cookieName = cookieName + '=';
+      var cookieData = document.cookie;
+      var start = cookieData.indexOf(cookieName);
+      var cookieValue = '';
+      if(start != -1){
+        start += cookieName.length;
+        var end = cookieData.indexOf(';', start);
+        if(end == -1)end = cookieData.length;
+        cookieValue = cookieData.substring(start, end);
+      }
+      return unescape(cookieValue);
+    }
